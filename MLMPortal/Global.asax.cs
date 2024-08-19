6d914a5a08d4267399818c03be51483994ef8bc3
VSS <varcassoftwaresolution@outlook.com>
2024-08-19 11:04:39 -0700

    Commit All

--- MLMPortal/Global.asax.cs
+++ MLMPortal/Global.asax.cs
@@ -15,21 +15,21 @@ namespace MLMPortal
         protected void Application_Start()
         {
             // Define the connection string
-            string connectionString = "Data Source=103.131.196.139;Initial Catalog=MissionPaymlmdb;User Id=mlmuser;Password=Varcas@mlm@2024";
+            //string connectionString = "Data Source=103.131.196.139;Initial Catalog=MissionPaymlmdb;User Id=mlmuser;Password=Varcas@mlm@2024";
 
             // Configure Hangfire
-            GlobalConfiguration.Configuration.UseSqlServerStorage(connectionString);
+            //GlobalConfiguration.Configuration.UseSqlServerStorage(connectionString);
 
             // Optional: Configure Hangfire to use a different job activation method if needed
             // GlobalConfiguration.Configuration.UseActivator(new MyJobActivator());
 
             // Start Hangfire server
-            Hangfire.GlobalConfiguration.Configuration.UseSqlServerStorage(connectionString);
-            Hangfire.BackgroundJobServerOptions serverOptions = new Hangfire.BackgroundJobServerOptions();
-            Hangfire.BackgroundJobServer server = new Hangfire.BackgroundJobServer(serverOptions);
+            //Hangfire.GlobalConfiguration.Configuration.UseSqlServerStorage(connectionString);
+            //Hangfire.BackgroundJobServerOptions serverOptions = new Hangfire.BackgroundJobServerOptions();
+            //Hangfire.BackgroundJobServer server = new Hangfire.BackgroundJobServer(serverOptions);
 
-            // Schedule a daily recurring job
-            RecurringJob.AddOrUpdate(() => MyBackgroundJob.DailyTask(), Cron.Daily);
+            //// Schedule a daily recurring job
+            //RecurringJob.AddOrUpdate(() => MyBackgroundJob.DailyTask(), Cron.Daily);
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
