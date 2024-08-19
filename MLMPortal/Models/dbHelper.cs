904a71b74eb0facc1be73a3dcc6c18b9cbca2ade
VSS <varcassoftwaresolution@outlook.com>
2024-08-19 11:05:43 -0700

    Commit All

--- MLMPortal/Models/dbHelper.cs
+++ MLMPortal/Models/dbHelper.cs
@@ -11,7 +11,7 @@ namespace MLMPortal.Models
     public class dbHelper
     {
         // private static string consString = "Data Source=DESKTOP-JADR545;Initial Catalog=MLMPortal1db;Integrated Security=True";
-         private static string consString = "Data Source=103.131.196.139;Initial Catalog=Sangam2mlmdb;User Id=mlmuser;Password=Varcas@mlm@2024";
+         private static string consString = "Data Source=103.131.196.139;Initial Catalog=Sangam2mlmdb;User Id=NewMLMUser;Password=mlmuser@2023##$";
 
         //private static string consString = "Data Source=DESKTOP-JADR545\\SQLEXPRESS;Initial Catalog=aunmoneyworldmlmdb;Integrated Security=True;";
         //private static string consString = "Data Source=LAPTOP-ETEAB50Q;Initial Catalog=aunmoneyworldmlmdb;Integrated Security=True;";
